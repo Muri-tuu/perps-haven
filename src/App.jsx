1d0140ea86f455bed2d24985e8f159cc426c2e29
Muri-tuu <kenhopkins001@gmail.com>
2025-11-22 01:26:23 +0300

    feat: setup core application structure and global styles

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { ShopProvider } from './context/ShopContext';
+import { ReviewProvider } from './context/ReviewContext';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import ProductList from './components/ProductList';
+import Reviews from './components/Reviews';
+import Footer from './components/Footer';
+import CartDrawer from './components/CartDrawer';
+import CheckoutModal from './components/CheckoutModal';
+import './index.css';
+
+import { ThemeProvider } from './context/ThemeContext';
+
+function App() {
+  return (
+    <ShopProvider>
+      <ThemeProvider>
+        <ReviewProvider>
+          <div className="app">
+            <Navbar />
+            <Hero />
+            <div id="gallery">
+              <ProductList />
+            </div>
+            <Reviews />
+            <Footer />
+            <CartDrawer />
+            <CheckoutModal />
+          </div>
+        </ReviewProvider>
+      </ThemeProvider>
+    </ShopProvider>
+  );
+}
+
+export default App;
